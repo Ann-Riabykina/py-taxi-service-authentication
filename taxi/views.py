bd37aa147d27d75df584062da8655910712d37d0
Ann Riabykina <anny.r20.1994@gmail.com>
2025-12-27 15:08:38 +0200

    Num visits fix

--- taxi/views.py
+++ taxi/views.py
@@ -11,7 +11,8 @@ def index(request):
     """View function for the home page of the site."""
 
     num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
+    num_visits += 1
+    request.session["num_visits"] = num_visits
 
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
