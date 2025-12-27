6d826fbf03168576776496f7a83b6739359ee838
Ann Riabykina <anny.r20.1994@gmail.com>
2025-12-27 15:06:26 +0200

    Urls fix

--- taxi/urls.py
+++ taxi/urls.py
@@ -24,8 +24,9 @@ urlpatterns = [
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
     path("accounts/login/", auth_views.LoginView.as_view(
-        template_name="taxi/login.html"), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+        template_name="registration/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(
+        template_name="registration/logged_out.html"), name="logout"),
 ]
 
 app_name = "taxi"
